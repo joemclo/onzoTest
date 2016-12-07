0b71b13acfcd3c1643e0d3b001aae7dad03ae003
Joe Mclo <joemclo8@gmail.com>
2016-12-07 20:04:40 +0000

    add better cleaning and styling builds to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,31 @@ const gulp = require("gulp");
 const nodemon = require("gulp-nodemon");
 const ts = require("gulp-typescript");
 const path = require("path");
+const sass = require("gulp-sass");
 const source = require('vinyl-source-stream');
 
 const tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("clean", (cb) => {
-        return del("./bin", cb);
+gulp.task("clean-ts", (cb) => {
+        return del(["./bin/**/*.js"], cb);
 })
 
-gulp.task("copyAssests", ["clean"], () => {
-  gulp.src(["./src/styling/**", "./src/index.html"])
+gulp.task("clean-assets", (cb) => {
+        return del(["./bin/**/*.css"], cb);
+})
+
+gulp.task("copyAssests", ["clean-assets"], () => {
+  gulp.src(["./src/index.html"])
     .pipe(gulp.dest('./bin/public/'));
 });
 
-gulp.task("compile-ts" , ["clean"],  () => {
+gulp.task("styling", ["clean-assets"], () => {
+        gulp.src(["./src/styling/*.{scss,sass}"])
+        .pipe(sass())
+        .pipe(gulp.dest("./bin/public/css"))
+})
+
+gulp.task("compile-ts" , ["clean-ts"],  () => {
     const source = "./src/**/*.{tsx,ts}";
 
     return tsProject.src()
@@ -35,16 +46,17 @@ gulp.task("bundle", ["compile-ts"], () => {
         .pipe(gulp.dest("./bin/public"));
 })
 
-gulp.task("build", ["bundle"]);
+gulp.task("build", ["bundle", "styling"]);
 
-gulp.task("nodemon", ["clean", "copyAssests", "bundle"], () => {
+gulp.task("nodemon", ["copyAssests", "bundle", "styling"], () => {
 
         return nodemon({
                 script: "./bin/server/index.js",
-                ext: "ts tsx",
+                ext: "ts tsx scss",
                 watch : [
-                        "src/**/*.ts",
-                        "src/**/*.tsx",
+                        "src/app/**/*.ts",
+                        "src/app/**/*.tsx",
+                        "src/styling/**/*.scss",
                 ],
                 tasks : (changedFiles) => {
                         console.log(changedFiles)
@@ -53,8 +65,11 @@ gulp.task("nodemon", ["clean", "copyAssests", "bundle"], () => {
                                 changedFiles.forEach((file) => {
                                         const extName = path.extname(file);
                                         if ((extName === ".ts" || extName === ".tsx") && !~tasks.indexOf("bundle")) {
-                                                tasks.push("bundle", "copyAssests");
+                                                tasks.push("bundle");
                                         }
+                                        if ((path.extname(file) === '.scss') && !~tasks.indexOf('styling')){
+                                                tasks.push('styling');
+                                        } 
                                 });
 
                         }
